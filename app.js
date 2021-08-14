774f6060f1b2394e6555100a01155abf60684260
MilenaMGodoy <69657162+MilenaMGodoy@users.noreply.github.com>
2021-08-14 17:58:08 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 let nombre = prompt("Ingresa tu nombre");
 let contrasenia = prompt("Ingresa tu contraseña");
 let anioNacimiento = Number(prompt("Ingresa tu año de nacimiento"));
@@ -19,3 +21,71 @@ if (contrasenia.toUpperCase() == "CODERHOUSE" && anioActual == 2021){
 else {
     alert ("la contraseña es incorrecta");
 }
+
+class Curso {
+    constructor(curso, duracion, precio) {
+        this.curso = curso;
+        this.duracion = duracion;
+        this.precio = precio
+    }
+}
+
+const catalogo = []
+
+
+
+const ingresarCurso = () => {
+    let curso = prompt("Ingresa el curso")
+    let duracion = prompt("Ingresa la duracion")
+    let precio = Number(prompt("Ingresa el valor"))
+
+    const curso1 = new Curso (curso, duracion, precio)
+    catalogo.push(curso1)
+
+    console.log(catalogo);
+}
+
+ingresarCurso() 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
